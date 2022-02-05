d1055af356e0967349fc65ca1dc545a22e19e320
rfluan <luanramosferr@outlook.com>
2022-02-05 17:37:23 -0300

    Create ex008.js

---
+++ exercicios/aula11/ex008.js
@@ -0,0 +1,9 @@
+var vel = 20
+console.log(`A velocidade do seu carro é de ${vel}Km/h.`)
+if (vel > 60) { // Condição simples
+    console.log(`Você ultrapassou a velocidade permitida, MULTADO`)
+}
+if (vel < 30) {
+    console.log(`Você passou a baixo da velocidade mÍnima, MULTADO`)
+}
+console.log(`Dirija sempre usando cinto de segurança`)
