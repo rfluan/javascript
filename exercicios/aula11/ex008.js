800596647ae20a35119124abf9b3df6984a8b358
rfluan <luanramosferr@outlook.com>
2022-02-07 19:39:49 -0300

    Update ex008.js

--- exercicios/aula11/ex008.js
+++ exercicios/aula11/ex008.js
@@ -4,6 +4,6 @@ if (vel > 60) { // Condição simples
     console.log(`Você ultrapassou a velocidade permitida, MULTADO`)
 }
 if (vel < 30) {
-    console.log(`Você passou a baixo da velocidade mÍnima, MULTADO`)
+    console.log(`Você passou a baixo da velocidade minima, MULTADO`)
 }
 console.log(`Dirija sempre usando cinto de segurança`)
