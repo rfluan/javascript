6c1f6a54ff7f24ddfbfb25037c74771e13cff3fc
rfluan <luanramosferr@outlook.com>
2022-02-07 20:27:12 -0300

    ex012

---
+++ exercicios/aula12/ex011.js
@@ -0,0 +1,10 @@
+var idade = 80
+
+console.log(`Sua idade é de ${idade} anos.`)
+if (idade < 16) {
+    console.log('Não vota')
+} else if (idade < 18 || idade > 65) {
+    console.log('Voto opcional')
+} else {
+    console.log('Voto obrigatório')
+}
--- exercicios/aula12/ex012.js
+++ exercicios/aula12/ex012.js
@@ -1,10 +1,15 @@
-var idade = 80
+var agora = new Date()
+var agora2 = new Date()
+var hora = agora.getHours()
+var min = agora2.getMinutes()
+console.log(`Agora são exatamente ${hora} horas e ${min} Minutos.`)
 
-console.log(`Sua idade é de ${idade} anos.`)
-if (idade < 16) {
-    console.log('Não vota')
-} else if (idade < 18 || idade > 65) {
-    console.log('Voto opcional')
+if (hora < 5) {
+    console.log('Boa Madrugada')
+} else if (hora < 12) {
+    console.log('Bom Dia')
+} else if (hora < 18) {
+    console.log('Boa tarde')
 } else {
-    console.log('Voto obrigatório')
+    console.log('Boa noite')
 }
